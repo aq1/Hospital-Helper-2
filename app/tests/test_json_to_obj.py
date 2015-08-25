a7b16b42fd3944dafeafea98cc2c743669e1ce87
aq1 <inmosh@yandex.ru>
2015-08-26 01:30:20 +0300

    Add test

--- app/tests/test_json_to_obj.py
+++ app/tests/test_json_to_obj.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import unittest
+import json
 
 from app.logic.json_to_obj import JsonToObj
 from app.logic.organ import Organ
@@ -11,6 +12,9 @@ class TestJsonToObj(unittest.TestCase):
     def setUp(self):
         self.klass = Organ
         self.json_to_obj = JsonToObj(Organ)
+
+        with open('structure.json', 'r', encoding='utf8') as f:
+            self.json = json.loads(f.read())
 
     def test_object_creation(self):
         args = {'name': 'TestOrgan',
@@ -27,7 +31,16 @@ class TestJsonToObj(unittest.TestCase):
         indexes = {'arg': 0}
         out = self.json_to_obj._str_to_expr(string=string, indexes=indexes)
 
-        self.assertEqual(out, 'self.arg + self._get_value_from_mediator("hello") - 4')
+        self.assertEqual(
+            out, 'self.arg + self._get_value_from_mediator("hello") - 4')
 
     def test_repr(self):
-        self.assertEqual(self.json_to_obj.__repr__(), self.json_to_obj.__class__.__name__)
+        self.assertEqual(
+            self.json_to_obj.__repr__(), self.json_to_obj.__class__.__name__)
+
+    def test_real_json(self):
+        organs = self.json['organs']
+        for organ in organs:
+            obj = JsonToObj(self.klass, organ).create_obj()
+            self.assertIn(organ['args'][0][0], obj.__dict__)
+            self.assertEqual(len(obj.args), len(organ['args']))
