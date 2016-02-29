ad3071e6aedde47fd27a6e5bab3ea57cb3667eed
aq1 <inmosh@yandex.ru>
2016-03-01 00:53:01 +0400

    Small step for the man a giant step for the project. Not really

--- app/gui/gui.py
+++ app/gui/gui.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import functools
 
 from PyQt5 import QtCore
 from PyQt5.QtCore import QCoreApplication, Qt
@@ -9,7 +10,7 @@ from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QDesktopWidget, QVBoxLa
                              QTextEdit, QGridLayout, QApplication, QHBoxLayout,
                              QFormLayout, QStyle, QShortcut, QStyle, QStyleOption,
                              QFrame, QPushButton, QScrollArea, QStackedLayout, QGridLayout,
-                             QGraphicsDropShadowEffect)
+                             QGraphicsDropShadowEffect, QScrollArea)
 
 from PyQt5.QtGui import (QKeySequence, QFont, QFontDatabase, QPainter)
 
@@ -41,6 +42,12 @@ class TopSystemButtons(QFrame):
         b.addWidget(exit_button)
         self.setLayout(b)
 
+        shadow = QGraphicsDropShadowEffect()
+        shadow.setBlurRadius(20)
+        shadow.setXOffset(0)
+        shadow.setYOffset(5)
+        self.setGraphicsEffect(shadow)
+
     def mousePressEvent(self, event):
         self.move_offset = event.pos()
 
@@ -66,15 +73,22 @@ class TopFrame(QFrame):
     Top Frame with decorative elements
     """
 
-    def __init__(self, main_window):
+    def __init__(self, main_window, items):
         super().__init__()
 
         self.main_window = main_window
+        vbox = QVBoxLayout()
+        vbox.setSpacing(0)
+        vbox.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(vbox)
+
+        vbox.addStretch()
+        vbox.addWidget(SelectMenu(main_window))
 
         shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(10)
+        shadow.setBlurRadius(20)
         shadow.setXOffset(0)
-        shadow.setYOffset(3)
+        shadow.setYOffset(5)
         self.setGraphicsEffect(shadow)
 
 
@@ -91,6 +105,7 @@ class Input(QFrame):
             return
 
         hbox = QHBoxLayout()
+        self.setLayout(hbox)
         hbox.addWidget(QLabel(_(label_text)))
         hbox.addStretch()
         e = QLineEdit()
@@ -103,19 +118,6 @@ class Input(QFrame):
         shadow.setYOffset(0)
         self.setGraphicsEffect(shadow)
         hbox.addWidget(e)
-
-        # self.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
-        # form = QFormLayout()
-        # form.setLabelAlignment(Qt.AlignRight)
-        # form.setAlignment(Qt.AlignRight)
-        # form.setFormAlignment(Qt.AlignRight | Qt.AlignTop)
-        # form.addRow(_(label_text), e)
-        # hbox = QHBoxLayout()
-        # hbox.addWidget(QLabel(label_text), stretch=20)
-        # hbox.addWidget(QLineEdit(), stretch=600)
-
-        # self.setLayout(form)
-        self.setLayout(hbox)
 
 
 class AttributesFrame(QWidget):
@@ -132,15 +134,14 @@ class AttributesFrame(QWidget):
 
         super().__init__()
         print(item)
+
         self.main_window = main_window
         hbox = QHBoxLayout()
+        self.setLayout(hbox)
         hbox.setSpacing(0)
         hbox.setContentsMargins(0, 0, 0, 0)
-        hbox.addSpacing(main_window.width() * 0.08)
-        # grid = QGridLayout()
-        # grid.setOriginCorner(Qt.TopLeftCorner)
+        hbox.addSpacing(25)
 
-        # col = -2
         rows = 5
         for i, arg_name in enumerate(item):
             if i % rows == 0:
@@ -151,20 +152,10 @@ class AttributesFrame(QWidget):
                 vbox = QVBoxLayout()
                 vbox.setSpacing(0)
                 vbox.setContentsMargins(0, 0, 0, 0)
-                # vbox.setFrameStyle(QFrame.StyledPanel | QFrame.Sunken)
+
                 hbox.addLayout(vbox)
 
             vbox.addWidget(Input(arg_name))
-            # if i % rows == 0:
-            #     col += 2
-
-            # l = QLabel('%s %s' % (i % rows, col))
-            # e = QLineEdit()
-            # e.setText('%s %s' % (i % rows, col + 1))
-            # l.setAlignment(Qt.AlignRight)
-
-            # grid.addWidget(l, i % rows, col, Qt.AlignLeft)
-            # grid.addWidget(e, i % rows, col + 1, Qt.AlignLeft)
 
         try:
             vbox.addStretch()
@@ -172,32 +163,42 @@ class AttributesFrame(QWidget):
             pass
 
         hbox.addStretch(100)
+
+
+class SelectMenu(QFrame):
+
+    BUTTON_SELECTED_QSS = "padding-bottom: 23px; border-bottom: 2px solid #FFEB3B;"
+
+    def __init__(self, main_window):
+
+        super().__init__()
+        self.main_window = main_window
+
+        hbox = QHBoxLayout()
+        hbox.setSpacing(0)
+        hbox.setContentsMargins(0, 0, 0, 0)
         self.setLayout(hbox)
-        # vbox = QVBoxLayout()
-        # vbox.setContentsMargins(0, 0, 0, 0)
-        # vbox.setSpacing(0)
 
-        # hbox = QHBoxLayout()
-        # hbox.setContentsMargins(0, 0, 0, 0)
-        # hbox.setSpacing(0)
-        # hbox.addSpacing(self.width() * 0.15)
+        hbox.addSpacing(25)
+        self.buttons = [QPushButton(_(text)) for text in main_window.MENU_LABELS]
 
-        # for i, x in enumerate(item):
-        #     if i % 6 == 0:
-        #         form = QFormLayout()
-        #         form.setLabelAlignment(Qt.AlignRight)
-        #         hbox.addLayout(form)
-        #     e = QLineEdit()
-        #     e.setAlignment(Qt.AlignRight)
-        #     form.addRow(_(x), e)
+        for i, btn in enumerate(self.buttons):
+            if i == 0:
+                # Data button needs special action
+                btn.enterEvent = self.main_window.data_button_entered
+                btn.setStyleSheet(self.BUTTON_SELECTED_QSS)
 
-        # hbox.addStretch(100)
-        # hbox.addSpacing(25)
-        # vbox.addWidget(TopSystemButtons(), stretch=3)
-        # vbox.addWidget(TopFrame(), stretch=15)
-        # vbox.addStretch(5)
-        # vbox.addLayout(hbox, stretch=40)
-        # self.setLayout(vbox)
+            btn.clicked.connect(functools.partial(self.button_clicked, btn))
+            hbox.addWidget(btn)
+
+        hbox.addStretch()
+
+    def button_clicked(self, btn, event=None):
+        for each in self.buttons:
+            each.setStyleSheet('')
+
+        btn.setStyleSheet(self.BUTTON_SELECTED_QSS)
+        self.main_window.select_menu_button_clicked(btn)
 
 
 class LeftMenu(QFrame):
@@ -244,17 +245,63 @@ class ActionButton(QFrame):
         shadow.setYOffset(0)
         self.setGraphicsEffect(shadow)
 
-        print(main_window.waterline)
+        # print(main_window.waterline)
         self.move(1300, 210)
 
 
+class DataWidget(QFrame):
+
+    def __init__(self, main_window, items):
+
+        super().__init__()
+
+        self.main_window = main_window
+        self.stacked_layout = QStackedLayout()
+        self.setLayout(self.stacked_layout)
+
+        for item in items[1:]:
+            frame = AttributesFrame(main_window=self, item=item)
+            self.stacked_layout.addWidget(frame)
+
+
+class ReportWidget(QFrame):
+
+    def __init__(self, main_window, items):
+
+        super().__init__()
+        hbox = QHBoxLayout()
+        self.setLayout(hbox)
+        hbox.addWidget(QLabel('Report widget'))
+
+
+class DBWidget(QFrame):
+
+    def __init__(self, main_window, *args):
+
+        super().__init__()
+        hbox = QHBoxLayout()
+        self.setLayout(hbox)
+        hbox.addWidget(QLabel('DB widget'))
+
+
+class OptionsWidget(QFrame):
+
+    def __init__(self, main_window, *args):
+
+        super().__init__()
+        hbox = QHBoxLayout()
+        self.setLayout(hbox)
+        hbox.addWidget(QLabel('Options widget'))
+
+
 class MainWindow(QWidget):
+
+    MENU_LABELS = 'data', 'report', 'base', 'options'
 
     def __init__(self, items):
         super().__init__()
 
         self.items = items
-        self.stacked_layout = QStackedLayout()
 
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setWindowTitle('Hospital Helper')
@@ -262,6 +309,7 @@ class MainWindow(QWidget):
         w = dw.geometry().size().width() * 0.75
         self.resize(w, w * 0.6)
 
+        self.stacked_layout = QStackedLayout()
         vbox = QVBoxLayout()
         vbox.setContentsMargins(0, 0, 0, 0)
         vbox.setSpacing(0)
@@ -269,15 +317,12 @@ class MainWindow(QWidget):
 
         vbox.addWidget(TopSystemButtons(self), stretch=3)
 
-        t = TopFrame(self)
+        t = TopFrame(self, items)
         vbox.addWidget(t, stretch=15)
         self.waterline = self.height() - t.height()
 
         vbox.addSpacing(50)
         vbox.addLayout(self.stacked_layout, stretch=40)
-
-        # LeftMenu(self, items)
-        ActionButton(self)
 
         qr = self.frameGeometry()
         cp = dw.availableGeometry().center()
@@ -286,19 +331,38 @@ class MainWindow(QWidget):
 
         self._create_layout()
 
-        self.i = 0
+        self.stack_index = 0
 
-        QShortcut(QKeySequence('Esc'), self).activated.connect(self.close)
+        self._set_shortcuts()
+
         self.show()
-    # def mouseReleaseEvent(self, event):
-    #     self.i = (self.i + 1) % len(self.items)
-    #     self.stacked_layout.setCurrentIndex(self.i)
+
+    def _set_shortcuts(self):
+        QShortcut(QKeySequence('Esc'), self).activated.connect(self.close)
+        QShortcut(Qt.CTRL, self).activated.connect(self.close)
 
     def _create_layout(self):
+        # Order matters
+        frames = DataWidget, ReportWidget, DBWidget, OptionsWidget
+        for frame in frames:
+            self.stacked_layout.addWidget(frame(self, self.items))
 
-        for item in self.items:
-            frame = AttributesFrame(main_window=self, item=item)
-            self.stacked_layout.addWidget(frame)
+    def data_button_entered(self, event):
+        # Event is triggered only on 'Data' tab
+        if self.stack_index:
+            return
+
+        print('HAHA')
+
+    def select_menu_button_clicked(self, btn):
+        for i, each in enumerate(self.MENU_LABELS):
+            if _(each) == btn.text():
+                self.stack_index = i
+                self.stacked_layout.setCurrentIndex(self.stack_index)
+
+    def keyPressEvent(self, event):
+        print(event.modifiers() == Qt.ControlModifier, event.text() is '')
+        super().keyPressEvent(event)
 
     def close(self, event=None):
         QCoreApplication.instance().quit()
