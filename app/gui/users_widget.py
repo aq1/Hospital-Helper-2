b284fa76be2ad4347de870b0e27f21b7e1656311
Vlad Grech <inmosh@yandex.ru>
2016-06-14 18:27:22 +0300

    Change query

--- app/gui/users_widget.py
+++ app/gui/users_widget.py
@@ -46,8 +46,8 @@ class UsersWidget(QFrame):
         """
 
         utils.clear_layout(self.content_layout)
-        self.users = db.SESSION.query(db.User).all()
-        organizations = db.SESSION.query(db.Organization).all()
+        self.users = db.SESSION.query(db.User).filter(db.User.deleted == False)
+        organizations = db.SESSION.query(db.Organization).filter(db.Organization.deleted == False)
 
         for organization in organizations:
             self.content_layout.addWidget(self._get_label(organization))
@@ -80,7 +80,7 @@ class UsersWidget(QFrame):
         """
         Select user.
         """
-        self.main_window.user_selected(user)
+        self.main_window.user_selected(user, go_to_data_frame=True)
 
     def action_btn_function(self):
         """
