dfc8a898d0e47bd3fa3608c3a0712f7b9eaa3a86
Vladimir Grechukhin <vgrechyukhin@ptsecurity.com>
2016-08-18 15:12:37 +0300

    Fix test

--- app/tests/test_allowed_module.py
+++ app/tests/test_allowed_module.py
@@ -8,4 +8,4 @@ class TestAllowedModule(unittest.TestCase):
     def test_module_storage(self):
         import math
         allowed_module = AllowedModule(math)
-        assert issubclass(allowed_module, list)
+        assert isinstance(allowed_module, list)
