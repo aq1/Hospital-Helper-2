99a9336d38d977d855a5366f084f35010ff567ce
Vladimir Grechukhin <vgrechyukhin@ptsecurity.com>
2016-08-19 18:56:06 +0300

    Start experiments with vagrant

---
+++ vagrant/linux.sh
@@ -0,0 +1,11 @@
+sudo -i
+
+apt-get update
+
+apt-get install git -y
+apt-get install python3-pip -y
+
+git clone https://github.com/aq1/Hospital-Helper-2.git /home/vagrant/hh
+cd /home/vagrant/hh
+
+pip3 install -r requirements.txt
