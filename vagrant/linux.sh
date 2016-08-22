20cf5d1c7a66c81f94489c6871f3986ca5417acf
Vladimir Grechukhin <vgrechyukhin@ptsecurity.com>
2016-08-22 18:13:42 +0300

    Add pyqt to linux.sh

--- vagrant/linux.sh
+++ vagrant/linux.sh
@@ -4,6 +4,7 @@ apt-get update
 
 apt-get install git -y
 apt-get install python3-pip -y
+apt-get install pyqt5-dev-tools -y
 
 git clone https://github.com/aq1/Hospital-Helper-2.git /home/vagrant/hh
 cd /home/vagrant/hh
